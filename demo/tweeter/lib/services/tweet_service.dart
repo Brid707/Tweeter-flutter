02a217db35677ed4a240962dca0de4a384c8c26d
Brid707 <bridmen18@outlook.com>
2026-03-26 22:55:53 -0600

    Use Render backend URL

--- demo/tweeter/lib/services/tweet_service.dart
+++ demo/tweeter/lib/services/tweet_service.dart
@@ -6,7 +6,7 @@ import '../models/tweet_response.dart';
 class TweetService {
   static final TweetService _instance = TweetService._internal();
 
-  final String baseUrl = 'http://localhost:8080/api';
+  final String baseUrl = 'https://tweeter-api-28or.onrender.com/api';
   late http.Client _httpClient;
 
   TweetService._internal() {
